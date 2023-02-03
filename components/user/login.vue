c1c738853637c398d8d6b5e61368923f22493ba5
maxprogit <sidewiki@live.com.mx>
2023-02-02 23:22:11 -0600

    conexion a backend

--- components/user/login.vue
+++ components/user/login.vue
@@ -1,17 +1,37 @@
 <template>
     <v-card class="cardLogin">
-        <v-card-title>Login</v-card-title>
+        <v-card-title class="title">Inicia Sesion</v-card-title>
         
         <v-card-text>
-            <v-form v-model="formularioValido">
-                <v-text-field label="Correo Electronico"
-                placeholder=" Correo Electronico" 
-                v-model="CorreoElectronico"
-                />
-            </v-form>
+            <v-row>
+                <v-col cols="4" align-self="center">
+                    <img src="../../assets/images/kenny.png" class="imgLogin">
+                </v-col>
+                <v-col cols="8" align-self="center">
+                    <v-form ref="formLogin">
+                        <v-text-field label="Correo Electronico"
+                        placeholder=" Correo Electronico" 
+                        v-model="CorreoElectronico"
+                        :rules="validarCorreo"
+                        />
+                        <v-text-field label="Password"
+                        placeholder="Password" 
+                        v-model="Password"
+                        :rules="validarPassword"
+                        />
+                    </v-form>
+                </v-col>
+            </v-row>
+           
         </v-card-text>
         <v-card-actions>
-            Botones para iniciar sesion
+            <v-btn class="btnLogin"
+                rounded
+                block
+                @click="loginBackend"
+            >
+                Iniciar Sesion
+            </v-btn>
         </v-card-actions>
     </v-card>
 </template>
@@ -20,8 +40,25 @@
     export default {
         data() {
             return {
-            formularioValido: false,
-            CorreoElectronico: ''
+            CorreoElectronico: '',
+            validarCorreo: [
+                v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+                ],
+            Password: '',
+            validarPassword: [
+                value => value.length >= 6 || 'Min 6 characters',
+                ]
+            }
+        },
+        methods: {
+            loginBackend () {
+                const valid = this.$refs.formLogin.validate()
+                if(valid){
+                    alert('Presionaste el boton')
+                } else {
+                    alert('No cumpliste con las reglas')
+                }
+                
             }
         }
     }
@@ -29,9 +66,23 @@
 
 <style scoped> 
     .cardLogin {
-        background-color: rgb(30, 100, 30);
+        background-color: rgb(99, 168, 99);
         border-radius: 10px;
         width: 500px;
         height: 300px;
+    }
+    .imgLogin {
+        width: 85%;
+        height: 85%;
+    }
+    .btnLogin {
+        background-color: grey!important;
+        color: rgb(126, 97, 53);
+    }
+    .title {
+        font-size: 25px;
+        color: rgb(126, 97, 53);
+        justify-content: center;
+        font-weight: 200;
     }
 </style>
